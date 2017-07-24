b986a5177223650ddeb0911392c314e3f797df10
Absolute (Ben .S) <absolute.intros@gmail.com>
2017-07-24 08:50:23 +0100

    Added WriteJMP & WriteNOP, made use of hooked bool

--- src/SimpleHook.cpp
+++ src/SimpleHook.cpp
@@ -30,21 +30,30 @@ BOOL Detour::Hooked() {
 	return hooked;
 }
 
-BOOL Detour::Create(DWORD size /* amount to write */, BOOL call /* set to true if a call should be placed instead of a jmp */) {
-	DWORD backup, oldprotect, offset = (DWORD)to_address - (DWORD)from_address - 5;
-	if (!VirtualProtect(from_address, size, PAGE_EXECUTE_READWRITE, &backup)) return FALSE;
-	memset(from_address, 0xE9 - call, 1);
-	memcpy((void *)((DWORD)from_address + 1), &offset, 4);
+BOOL Detour::WriteJMP(void * from, void * to, BOOL call, DWORD size = 5) {
+	DWORD backup, oldprotect, offset = (DWORD)to - (DWORD)from - 5;
+	if (!VirtualProtect(from, size, PAGE_EXECUTE_READWRITE, &backup)) return FALSE;
+	memset(from, 0xE9 - call, 1);
+	memcpy((void *)((DWORD)from + 1), &offset, 4);
 	for (int i = 0; i < size - 5; i++)
-		memset((void *)((DWORD)from_address + 6 + i), 0x90, 1);
-	if (!VirtualProtect(from_address, size, backup, &oldprotect)) return FALSE;
+		memset((void *)((DWORD)from + 6 + i), 0x90, 1);
+	if (!VirtualProtect(from, size, backup, &oldprotect)) return FALSE;
+}
+
+BOOL Detour::WriteNOP(void * at, DWORD size) {
+	DWORD backup, oldprotect;
+	if (!VirtualProtect(at, 5, PAGE_EXECUTE_READWRITE, &backup)) return FALSE;
+	for (int i = 0; i < size; i++)
+		memset((void *)((DWORD)at + i), 0x90, 1);
+	if (!VirtualProtect(at, 5, backup, &oldprotect)) return FALSE;
 	return TRUE;
 }
 
+BOOL Detour::Create(DWORD size /* amount to write */, BOOL call /* set to true if a call should be placed instead of a jmp */) {
+	hooked = WriteJMP(from_address, to_address, call, size);
+	return hooked;
+}
+
 BOOL Detour::Remove() {
-	DWORD backup, oldprotect;
-	if (!VirtualProtect(from_address, 5, PAGE_EXECUTE_READWRITE, &backup)) return FALSE;
-	if (!WriteProcessMemory(GetCurrentProcess(), (PVOID)from_address, "\x90\x90\x90\x90\x90", 5, NULL))
-		if (!VirtualProtect(from_address, 5, backup, &oldprotect)) return FALSE;
-	return TRUE;
+	return WriteNOP(from_address, 5);
 }
